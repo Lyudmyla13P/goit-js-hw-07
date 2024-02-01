3012e23244439a0a54d719a59abd96b49b9a28a9
Люда Парубок <lyudaparubok@MacBook-Air-Luda.local>
2024-02-01 15:24:25 +0200

    up

--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,8 @@
-const changeColorButton = document.querySelector('.change-color');
-const bodyElement = document.body;
-const colorSpan = document.querySelector('.color');
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
 changeColorButton.addEventListener('click', function () {
   const randomColor = getRandomHexColor();
 
