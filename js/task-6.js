d6b526a021e451926f59af339e3ca48e01cf8cba
Люда Парубок <lyudaparubok@MacBook-Air-Luda.local>
2024-02-01 15:24:29 +0200

    up

--- js/task-6.js
+++ js/task-6.js
@@ -1,14 +1,8 @@
-const input = document.querySelector('input[type="number"]');
-const createButton = document.querySelector('button[data-create]');
-const destroyButton = document.querySelector('button[data-destroy]');
-const boxesContainer = document.getElementById('boxes');
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
 function createBoxes(amount) {
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
