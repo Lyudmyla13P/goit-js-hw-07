fa9ac0c77064b1f228ce6f497fa1e6a2895e1068
Люда Парубок <lyudaparubok@MacBook-Air-Luda.local>
2024-02-01 11:51:39 +0200

    up

---
+++ js/task-6.js
@@ -0,0 +1,37 @@
+const input = document.querySelector('input[type="number"]');
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createButton.addEventListener('click', function () {
+  const numberOfBoxes = parseInt(input.value);
+
+  if (numberOfBoxes >= 1 && numberOfBoxes <= 100) {
+    createBoxes(numberOfBoxes);
+    input.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
